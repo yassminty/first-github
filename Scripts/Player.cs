cd96ff16c741b4dd7117e58882e61b8b4543957d
yassminty <s10247871@connect.np.edu.sg>
2024-07-05 23:16:16 +0800

    added some stuff with script,assets&audio

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
-    // Start is called before the first frame update
+{/// <summary>
+/// urm, coding for player health and hit damage
+/// </summary>
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    // game will start with this amount of health
     void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // if the player collides with danger he will lose health
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void TakeDamage(int damage)
     {
-        
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
     }
 }
